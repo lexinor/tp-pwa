f77920e7d90f6dc3b3b8028f75826268b842b8d6
Alessandro Alterno <alternonolimit@gmail.com>
2020-05-31 22:25:47 +0200

    service worker 'activate' now removes old cache

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # tp-pwa
 
 TP réalisé en 4ème année de mon Master Mobilité et Objets Connectés à l'ESGI
-
--- sw.js
+++ sw.js
@@ -37,7 +37,18 @@ self.addEventListener('install', event => {
 // TODO: 2c - On activation, remove obsolete caches
 
 self.addEventListener('activate', event => {
-    console.log('Your service worker has been activated');
+    console.log('Your service worker has been activated, cleaning old caches..');
+    event.waitUntil(
+        caches.keys().then( cacheNames => {
+            return Promise.all(
+                cacheNames.filter(cacheName => {
+                    return true;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', event => {
